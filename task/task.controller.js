80aa6768663803ab1974f580dbaf882ad9d30c55
kamlesh <kamleshbishnoi812@gmail.com>
2020-02-27 00:25:06 +0530

    drop functionality updated

--- task/task.controller.js
+++ task/task.controller.js
@@ -1,56 +1,67 @@
-
 const Detail = require("./task.model");
 
 // api for Sending task detail
 exports.sendTaskDetails = async (req, res) => {
-    if (req.body) {
-      try {
-        let retDoc = new Detail(req.body);
-        await retDoc.save();
-        return res.json(retDoc);
-      } catch (err) {
-        return res.json({ success: true, data: "", message: err });
-      }
-    } else {
-      return res.json({ success: false, data: "", message: "Parameter Missing" });
+  if (req.body) {
+    try {
+      let retDoc = new Detail(req.body);
+      let count = await Detail.count();
+      retDoc.Sno = count + 1;
+      await retDoc.save();
+      return res.json(retDoc);
+    } catch (err) {
+      return res.json({ success: true, data: "", message: err });
     }
-  };
+  } else {
+    return res.json({ success: false, data: "", message: "Parameter Missing" });
+  }
+};
 
-  // api for get task detail
-  exports.getTaskDetail=async(req,res)=>{
-    
-      let getTaskDetail= await Detail.find();
-      return res.json({ success: true, data: getTaskDetail, message: "found" });
-    
-  }
-  exports.updateTaskDetail=async(req,res)=>{
-    if(req.body.category && req.body._id){
-      try{
-      let updateTaskDetail= await Detail.findOneAndUpdate({_id:req.body._id},{$set:{category:req.body.category}},{new:true});
-      await updateTaskDetail.save();
+// api for get task detail
+exports.getTaskDetail = async (req, res) => {
+  let getTaskDetail = await Detail.find().sort({ Sno: 1 });
+  return res.json({ success: true, data: getTaskDetail, message: "found" });
+};
+exports.updateTaskDetail = async (req, res) => {
+  if (req.body.category && req.body._id) {
+    try {
+      let changes = [];
+      let updateTaskDetail = await Detail.findOneAndUpdate(
+        { _id: req.body._id },
+        { $set: { category: req.body.category } },
+
+        { new: true }
+      );
+      if (Array.isArray(req.body.SnoChange)) {
+        for (let item of req.body.SnoChange) {
+          let change = await Detail.findOneAndUpdate(
+            { _id: item._id },
+            { $set: { Sno: item.Sno } },
+            { new: true }
+          );
+          changes.push(change);
+        }
+      }
+      updateTaskDetail.changes = changes;
       return res.json(updateTaskDetail);
+    } catch (err) {
+      return res.json({ success: false, data: "", message: err });
     }
-    catch(err){
-      return res.json({success:false,data:"",message:"error"})
+  } else {
+    return res.json({ success: false, message: "error.." });
+  }
+};
+// api for delete task
+exports.deleteTask = async (req, res) => {
+  if (req.body._id) {
+    try {
+      let deleteTask = await Detail.findOneAndDelete({ _id: req.body._id });
+      return res.json({ success: true, data: deleteTask });
+    } catch (err) {
+      return res.json({ success: false, data: "", message: err });
     }
+  } else {
+    return res.json({ success: false, data: "", message: "parameter missing" });
   }
-  else{
-    return res.json({success:false,message:"error.."})
-  }
-  }
-   // api for delete task 
-  exports.deleteTask=async(req,res)=>{
-    if(req.body._id){
-     
-      try{
-        let deleteTask= await Detail.findOneAndDelete({_id:req.body._id});
-        return res.json({success:true,data:deleteTask})
-      }
-      catch(err){
-        return res.json({success:false,data:"",message:err})
-      }
-    }
-    else{
-      return res.json({success:false,data:"", message:"parameter missing"})
-    }
-  }
+};
+
--- task/task.model.js
+++ task/task.model.js
@@ -1,11 +1,18 @@
 var mongoose=require("mongoose");
+const AutoIncrementFactory = require('mongoose-sequence');
+ 
+// const connection = await mongoose.createConnection('mongodb://...');
+ 
+const AutoIncrement = AutoIncrementFactory(mongoose);
 var Schema=mongoose.Schema;
 var newSchema= new Schema({
     task:{type:String},
     category:{type:String},
     dueDate:{type:String},
+    Sno:{type:Number},
     creationDate: {type: Date, default: new Date()},
     updationDate: {type: Date, default: new Date()},
     
 });
+newSchema.plugin(AutoIncrement, {inc_field: 'Sno', disable_hooks: true});
 module.exports=mongoose.model("task",newSchema);
